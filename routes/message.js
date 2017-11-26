39d3774908d2a242531027256053dc6e2cc5ba4d
DESKTOP-JS8DOEJ\jiharu <kukaro4515@gmail.com>
2017-11-26 22:16:40 +0900

    #test routes/message.js print req.body

--- routes/message.js
+++ routes/message.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function (req, res, next) {
-    console.log(req);
+    console.log(req.body);
     var json = {
-        "message": {
-            "text": "아직 준비중입니다."
+        'message': {
+            'text': "아직 준비중입니다.\n"+
+                '입력하신 메시지 :'+req.body.toString()
         }
     };
     res.send(json);
