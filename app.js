5cda0a3498164fd67c95586a2917de1f751f6e02
DESKTOP-JS8DOEJ\jiharu <kukaro4515@gmail.com>
2017-11-26 22:01:34 +0900

    add message

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var keyboard = require('./routes/keyboard');
+var message = require('./routes/message');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/keyboard',keyboard);
+app.use('/message', message);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
