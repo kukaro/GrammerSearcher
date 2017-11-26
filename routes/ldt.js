7462b73a317fd71228bc4cc8ecb38698144d5656
DESKTOP-JS8DOEJ\jiharu <kukaro4515@gmail.com>
2017-11-27 06:18:19 +0900

    #test module routes/ldt.js - test LevelDB

--- routes/ldt.js
+++ routes/ldt.js
@@ -10,40 +10,18 @@ var db = level(dbPath);
 router.get('/', function (req, res, next) {
     var data;
 
-    var task1 = function (callback) {
-        /*db.put('name', 'LevelUP', function (err) {
+        db.put('ThisIsKey', 'ThisIsValue', function (err) {
             if(err) {
                 console.log(err);
-                callback(null);
             }
-            else {
-                console.log('put success');
-                callback(null);
-            }
-        });*/
-        callback(null);
-    };
-
-    var task2 = function (callback) {
-        db.get('name', function (err, value) {
-            if (err) return console.log(err);
-            console.log('name=' + value);
-            data = value;
-            callback(null);
+            db.get('ThisIsKey', function (err, value) {
+                if (err) {
+                    console.log(err);
+                }
+                console.log(value);
+                res.send(value);
+            });
         });
-    };
-
-    var task3 = function (callback) {
-        console.log('data', data);
-        res.send(data);
-        callback(null);
-    };
-
-    var tasks = [task1, task2, task3];
-    async.series(tasks, function (err, results) {
-        if (err) return console.log(err);
-        console.log('results', results);
-    });
 });
 
 module.exports = router;
