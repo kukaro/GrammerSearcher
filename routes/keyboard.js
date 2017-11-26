7703796d5797edd8094c6b1206375e023b089b38
DESKTOP-JS8DOEJ\jiharu <kukaro4515@gmail.com>
2017-11-27 07:54:59 +0900

    #edit routes/keyboard.js -remove levelDB routin #edit routes/message.js -add levelDB routin

--- routes/keyboard.js
+++ routes/keyboard.js
@@ -1,47 +1,9 @@
 var express = require('express');
-var async = require('async');
-var level = require('level');
-
-/*use levelDB as MyyDB*/
-var path = require('path');
-var dbPath = path.join(__dirname, 'mydb');
-var db = level(dbPath);
-
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
     var json = {'type': 'text'};
-    console.log('keyboard', req.body);
-
-    var task1 = function (callback) {
-        db.put(req.body.user_key, req.body.content, function (err) {
-            if (err) {
-                console.log(err);
-            }
-            callback(null);
-        });
-    };
-
-    var task2 = function (callback) {
-        db.put(req.body.user_key, function (err, value) {
-            if (err) {
-                console.log(err);
-            }
-            console.log(req.body.user_key, value);
-            callback(null);
-        });
-    };
-
-    var task3 = function (callback) {
-        res.send(json);
-        callback(null);
-    };
-
-    var tasks = [task1, task2, task3];
-
-    async.series(tasks,function (err, results) {
-        console.log(results);
-    });
+    res.send(json);
 });
 
 module.exports = router;
--- routes/message.js
+++ routes/message.js
@@ -1,17 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
+var level = require('level');
+
+/*use levelDB as MyyDB*/
+var path = require('path');
+var dbPath = path.join(__dirname, 'mydb');
+var db = level(dbPath);
 
 router.post('/', function (req, res, next) {
-    var userMessage = req.body;
-
+    var userMessage = userMessage;
     console.log(userMessage);
-    var json = {
-        'message': {
-            'text': "아직 준비중입니다.\n"+
-                '입력하신 메시지 :'+userMessage.content
-        }
+
+    const task1 = function (callback) {
+        db.put(userMessage.user_key, userMessage.content, function (err) {
+            if (err) {
+                console.log(err);
+            }
+            callback(null);
+        });
     };
-    res.send(json);
+
+    const task2 = function (callback) {
+        db.put(userMessage.user_key, function (err, value) {
+            if (err) {
+                console.log(err);
+            }
+            console.log(userMessage.user_key, value);
+            callback(null);
+        });
+    };
+
+    const task3 = function (callback) {
+        var json = {
+            'message': {
+                'text': "아직 준비중입니다.\n" +
+                '입력하신 메시지 :' + userMessage.content
+            }
+        };
+        res.send(json);
+        callback(null);
+    };
+
+    const tasks = [task1, task2, task3];
+
+    async.series(tasks, function (err, results) {
+        console.log(results);
+    });
+
 });
 
 module.exports = router;
