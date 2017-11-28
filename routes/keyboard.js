15569e046e4e58ca3da80df52fcff93b3440513b
DESKTOP-JS8DOEJ\jiharu <kukaro4515@gmail.com>
2017-11-28 13:22:50 +0900

    #test routes/keybaoard.js -test url

--- routes/keyboard.js
+++ routes/keyboard.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
+    console.log('url keyboard : ', req.body);
     var json = {'type': 'text'};
     res.send(json);
 });
